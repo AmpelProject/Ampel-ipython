dcf9a57435cd8ff087429fd6c4298321bb12665f
vbrinnel <valery.brinnel@gmail.com>
2022-02-06 18:40:36 +0100

    Update header

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-ipython/setup.py
-# License           : BSD-3-Clause
-# Author            : vb <vbrinnel@physik.hu-berlin.de>
-# Date              : 30.07.2020
-# Last Modified Date: 30.07.2020
-# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+# File:                Ampel-ipython/setup.py
+# License:             BSD-3-Clause
+# Author:              valery brinnel <firstname.lastname@gmail.com>
+# Date:                30.07.2020
+# Last Modified Date:  30.07.2020
+# Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from setuptools import setup # type: ignore
 
